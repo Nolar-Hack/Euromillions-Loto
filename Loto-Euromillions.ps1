66774312822444472ce9b87d584cd67c586a4121
Nolar-Hack <36194625+Nolar-Hack@users.noreply.github.com>
2023-12-08 23:07:48 +0100

    Update to put english values
    english value

--- Loto-Euromillions.ps1
+++ Loto-Euromillions.ps1
@@ -1,16 +1,17 @@
 #Install-Module -Name ImportExcel
 #Install-Module -Name ImportExcel -Scope CurrentUser -Verbose
 
-$ImportExcel = Import-Excel -Path D:\Project\Programs\Powershell\Base.xlsx -WorksheetName "Mes Numéros"
+$ImportExcel = Import-Excel -Path D:\Project\Programs\Powershell\Base.xlsx -WorksheetName "My Numbers"
 $ResultLotoNumber = Import-Excel -Path D:\Project\Programs\Powershell\Base.xlsx -WorksheetName "Tirage-Loto"
 $ResultEuroMillionsNumber = Import-Excel -Path D:\Project\Programs\Powershell\Base.xlsx -WorksheetName "Tirage-EuroMillions"
+$ResultEuroDreamsNumber = Import-Excel -Path D:\Project\Programs\Powershell\Base.xlsx -WorksheetName "Tirage-EuroDreams"
 
 
 Foreach($values in $ImportExcel) 
 {
 
     $MyArray = @{
-        Source=$values.Source;First=$values.'1er n°';Second=$values.'2eme n°';Third=$values.'3eme n°';Forth=$values.'4eme n°';Fifth=$values.'5eme n°';Comp1=$values.'n° chance 1';Comp2=$values.'n° chance 2'
+        Source=$values.Source;First=$values.'First Number';Second=$values.'Second Number';Third=$values.'Third Number';Forth=$values.'Fourth Number';Fifth=$values.'Fifth Number';sixth=$values.'Sixth Number';Comp1=$values.'n° chance 1';Comp2=$values.'n° chance 2'
     }
 
     $Source = $values.Source
@@ -19,6 +20,7 @@ Foreach($values in $ImportExcel)
     $ThirdMyNumber = $MyArray.Third
     $ForthMyNumber = $MyArray.Forth
     $FifthMyNumber = $MyArray.Fifth
+    $SixthMyNumber = $MyArray.Sixth
     $CompMyNumber1 = $MyArray.Comp1
     $CompMyNumber2 = $MyArray.Comp2
 
@@ -37,14 +39,14 @@ Foreach($values in $ImportExcel)
                 Foreach($elementLoto in $ResultLotoNumber) 
                 {
 
-                $DayLoto =  $elementLoto.Jour
-                $MonthLoto =  $elementLoto.Mois
-                $YearLoto =  $elementLoto.Année
-                $FirstNumberLoto = $elementLoto.'1er n°'
-                $SecondtNumberLoto = $elementLoto.'2eme n°'
-                $ThirdNumberLoto = $elementLoto.'3eme n°'
-                $ForthNumberLoto = $elementLoto.'4eme n°'
-                $FifthNumberLoto = $elementLoto.'5eme n°'
+                $DayLoto =  $elementLoto.Day
+                $MonthLoto =  $elementLoto.Month
+                $YearLoto =  $elementLoto.Year
+                $FirstNumberLoto = $elementLoto.'First Number'
+                $SecondtNumberLoto = $elementLoto.'Second Number'
+                $ThirdNumberLoto = $elementLoto.'Third Number'
+                $ForthNumberLoto = $elementLoto.'Fourth Number'
+                $FifthNumberLoto = $elementLoto.'Fifth Number'
                 $CompNumberLoto = $elementLoto.'n° chance 1'
                 $ResultArrayLoto = @($FirstNumberLoto,$SecondtNumberLoto,$ThirdNumberLoto,$ForthNumberLoto,$FifthNumberLoto)
 
@@ -85,14 +87,14 @@ Foreach($values in $ImportExcel)
                 Foreach($elementEuromillions in $ResultEuromillionsNumber) 
                 {
         
-                $DayEuromillions =  $elementEuromillions.Jour
-                $MonthEuromillions =  $elementEuromillions.Mois
-                $YearEuromillions =  $elementEuromillions.Année
-                $FirstNumberEuromillions = $elementEuromillions.'1er n°'
-                $SecondtNumberEuromillions = $elementEuromillions.'2eme n°'
-                $ThirdNumberEuromillions = $elementEuromillions.'3eme n°'
-                $ForthNumberEuromillions = $elementEuromillions.'4eme n°'
-                $FifthNumberEuromillions = $elementEuromillions.'5eme n°'
+                $DayEuromillions =  $elementEuromillions.Day
+                $MonthEuromillions =  $elementEuromillions.Month
+                $YearEuromillions =  $elementEuromillions.Year
+                $FirstNumberEuromillions = $elementEuromillions.'First Number'
+                $SecondtNumberEuromillions = $elementEuromillions.'Second Number'
+                $ThirdNumberEuromillions = $elementEuromillions.'Third Number'
+                $ForthNumberEuromillions = $elementEuromillions.'Fourth Number'
+                $FifthNumberEuromillions = $elementEuromillions.'Fifth Number'
                 $CompNumberEuromillions1 = $elementEuromillions.'n° chance 1'
                 $CompNumberEuromillions2 = $elementEuromillions.'n° chance 2'
                 $ResultArrayEuromillions = @($FirstNumberEuromillions,$SecondtNumberEuromillions,$ThirdNumberEuromillions,$ForthNumberEuromillions,$FifthNumberEuromillions)
@@ -115,6 +117,52 @@ Foreach($values in $ImportExcel)
                         Else
                             {
                             Write-Host "Dommage, tu t'es trompé sur les deux numéros complémentaires, il s'agissait du combo du $DayEuromillions $MonthEuromillions $YearEuromillions avec le numéro $ResultArrayEuromillions $ResultArrayEuromillionscomps"
+                            }
+                        }
+                    Else
+                        {
+
+                        }
+                }
+        }
+        ElseIf ($Source -match "EuroDreams")
+        {
+            $FirstMyNumberEurodreams = $FirstMyNumber
+            $SecondtMyNumberEurodreams = $SecondtMyNumber
+            $ThirdMyNumberEurodreams = $ThirdMyNumber
+            $ForthMyNumberEurodreams = $ForthMyNumber
+            $FifthMyNumberEurodreams = $FifthMyNumber
+            $SixthMyNumberEurodreams = $SixthMyNumber
+            $CompMyNumberEurodreams1 = $CompMyNumber1
+            $MyArrayEurodreams = @($FirstMyNumberEurodreams,$SecondtMyNumberEurodreams,$ThirdMyNumberEurodreams,$ForthMyNumberEurodreams,$FifthMyNumberEurodreams,$SixthMyNumberEurodreams)
+            $MyArrayEurodreamsComps = @($CompMyNumberEurodreams1)
+            $MyArrayEurodreamsALL = @($FirstMyNumberEurodreams,$SecondtMyNumberEurodreams,$ThirdMyNumberEurodreams,$ForthMyNumberEurodreams,$FifthMyNumberEurodreams,$SixthMyNumberEurodreams,$CompMyNumberEurodreams1)
+
+                Foreach($elementEurodreams in $ResultEurodreamsNumber) 
+                {
+        
+                $DayEurodreams =  $elementEurodreams.Day
+                $MonthEurodreams =  $elementEurodreams.Month
+                $YearEurodreams =  $elementEurodreams.Year
+                $FirstNumberEurodreams = $elementEurodreams.'First Number'
+                $SecondtNumberEurodreams = $elementEurodreams.'Second Number'
+                $ThirdNumberEurodreams = $elementEurodreams.'Third Number'
+                $ForthNumberEurodreams = $elementEurodreams.'Fourth Number'
+                $FifthNumberEurodreams = $elementEurodreams.'Fifth Number'
+                $SixthNumberEurodreams = $elementEurodreams.'Sixth Number'
+                $CompNumberEurodreams1 = $elementEurodreams.'n° chance 1'
+                $ResultArrayEurodreams = @($FirstNumberEurodreams,$SecondtNumberEurodreams,$ThirdNumberEurodreams,$ForthNumberEurodreams,$FifthNumberEurodreams,$SixthNumberEurodreams)
+                $ResultArrayEurodreamscomps = @($CompNumberEurodreams1)
+        
+                    If(($FirstMyNumberEurodreams -in $ResultArrayEurodreams) -and ($SecondtMyNumberEurodreams -in $ResultArrayEurodreams) -and ($ThirdMyNumberEurodreams -in $ResultArrayEurodreams) -and ($ForthMyNumberEurodreams -in $ResultArrayEurodreams) -and ($FifthMyNumberEurodreams -in $ResultArrayEurodreams) -and ($SixthMyNumberEurodreams -in $ResultArrayEurodreams))
+                        {
+                        if($CompMyNumberEurodreams1 -in $ResultArrayEurodreamscomps)
+                            {
+                            Write-Host "\o/ Niceluuuu ton numéro a été trouvé le $DayEurodreams $MonthEurodreams $YearEurodreams sur les resultats de l'Eurodreams \o/"
+                            }
+                        Else
+                            {
+                            Write-Host "Presque, mais tu t'es trompé sur le numéro complémentaire , il s'agissait du combo du $DayEurodreams $MonthEurodreams $YearEurodreams avec le numéro $ResultArrayEurodreams $ResultArrayEurodreamscomps"
                             }
                         }
                     Else
